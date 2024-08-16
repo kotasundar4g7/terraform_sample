a182396ea8098a1193876dd05443ecfdfd6275ce
kota.reddy <kota.reddy@happiestminds.com>
2024-08-16 12:07:03 +0530

    intial

---
+++ main.tf
@@ -0,0 +1,32 @@
+# main.tf
+
+# Specify the AWS provider
+provider "aws" {
+  region = "us-west-2"  # Change to your desired region
+}
+
+# Create an S3 bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-sample-bucket-playgound"  # Replace with your unique bucket name
+  acl    = "private"  # Set the access control list (e.g., private, public-read)
+
+  # Enable versioning
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  # Tagging the bucket
+  tags = {
+    Name        = "My S3 Bucket"
+    Environment = "Dev"
+  }
+}
