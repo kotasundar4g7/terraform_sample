ae7d89ddb1b97dac844b7a00a8c214028de0631b
kota.reddy <kota.reddy@happiestminds.com>
2024-08-23 13:44:52 +0530

    new change

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # Specify the provider (AWS in this case)
 provider "aws" {
-  region = "us-east-1" # Set your desired AWS region
+  region = "ap-south-1" # Set your desired AWS region
 }
 
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-sample-bucket-playgound2" # The bucket name must be globally unique
+  bucket = "my-sample-bucket-playgound3" # The bucket name must be globally unique
   acl    = "private"               # Set the bucket's ACL (e.g., private, public-read)
 
   # Enable versioning
