00a931fb4eded26c6fd82d1197f58c90a79ab797
kota.reddy <kota.reddy@happiestminds.com>
2024-08-23 13:47:23 +0530

    new change

--- main.tf
+++ main.tf
@@ -9,43 +9,10 @@ resource "aws_s3_bucket" "my_bucket" {
   acl    = "private"               # Set the bucket's ACL (e.g., private, public-read)
 
   # Enable versioning
-  versioning {
-    enabled = true
-  }
 
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 
   tags = {
     Name        = "My S3 Bucket"
     Environment = "Dev"
   }
-}
-
-# Optional: Set up a bucket policy (replace with your policy)
-resource "aws_s3_bucket_policy" "my_bucket_policy" {
-  bucket = aws_s3_bucket.my_bucket.bucket
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "s3:*",
-      "Effect": "Allow",
-      "Resource": [
-        "${aws_s3_bucket.my_bucket.arn}",
-        "${aws_s3_bucket.my_bucket.arn}/*"
-      ],
-      "Principal": "*"
-    }
-  ]
-}
-POLICY
 }
