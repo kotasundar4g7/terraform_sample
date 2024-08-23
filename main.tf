e65607e18277f3ac6b9fed39583d3ae9f485fde7
kota.reddy <kota.reddy@happiestminds.com>
2024-08-23 14:08:59 +0530

    new change

--- main.tf
+++ main.tf
@@ -2,12 +2,13 @@ provider "aws" {
   region = "ap-south-1"  # Replace with your region
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-sample-bucket-playgound3"
-}
+# The S3 bucket resource is removed/commented out since the bucket already exists
+# resource "aws_s3_bucket" "example" {
+#   bucket = "your-existing-bucket-name"
+# }
 
 resource "aws_s3_bucket_lifecycle_configuration" "example" {
-  bucket = aws_s3_bucket.example.bucket
+  bucket = "my-sample-bucket-playgound3"
 
   rule {
     id     = "log"
