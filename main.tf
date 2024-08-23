1d4c1d64f01263495f0a648f72f69793a265e253
kota.reddy <kota.reddy@happiestminds.com>
2024-08-23 13:54:45 +0530

    new change

--- main.tf
+++ main.tf
@@ -16,3 +16,27 @@ resource "aws_s3_bucket" "my_bucket" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "example" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+
+  rule {
+    id     = "Transition to Intelligent-Tiering"
+    status = "Enabled"
+
+    transition {
+      days          = 30
+      storage_class = "INTELLIGENT_TIERING"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "INTELLIGENT_TIERING"
+    }
+
+    expiration {
+      expired_object_delete_marker = true
+    }
+  }
+}
+
