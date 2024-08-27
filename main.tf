5a9b70aa7e4b7f991a32f176fd1171b512a63fec
kota.reddy <kota.reddy@happiestminds.com>
2024-08-27 11:28:06 +0530

    new s3 cycle

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "example" {
       storage_class = "GLACIER_IR"
     }
     transition {
-      days          = 91  # Number of days after object creation when it should transition to Glacier instant retrieval
+      days          = 120  # Number of days after object creation when it should transition to Glacier instant retrieval
       storage_class = "DEEP_ARCHIVE"
     }
   }
